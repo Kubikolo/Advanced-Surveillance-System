3b105fd83dae1bdfa327ea3d71571d1141e4641c
Kubikolo <jakub.kolotylo@gmail.com>
2026-05-06 19:06:16 +0200

    fixed consutrctor error

--- backend/json_parser.py
+++ backend/json_parser.py
@@ -20,12 +20,12 @@ def parameters_parser(filename):
     with open(filename) as json_file:
         parameters = json.load(json_file)
     objects = [[(x, y) for x, y in inner] for inner in parameters["objects"]]
-    drones = [Drone((drone["starting_position"][0], drone["starting_position"][1]), drone["radius"], drone["tickspeed"]) for drone in parameters["drones"]]
+    drones = [Drone((drone["starting_position"][0], drone["starting_position"][1]), drone["tickspeed"], drone["radius"]) for drone in parameters["drones"]]
     dimensions = (parameters["dimensions"][0], parameters["dimensions"][1])
     return {"objects":objects, "drones":drones, "dimensions":dimensions}
 
 def simulation_json_generator(filename, initial_steps, loop_steps, parameters):
-    with open(f"../shared/{filename}", 'w') as json_file:
+    with open(filename, 'w') as json_file:
         json.dump({
                     "initial_steps": initial_steps,
                     "loop_steps": loop_steps,
@@ -40,7 +40,8 @@ def simulation_json_generator(filename, initial_steps, loop_steps, parameters):
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    parameters = parameters_parser(f"../shared/parameters_{filename}")
+    parameters = parameters_parser(f"../shared/parameters_{filename}.json")
     waypoints = generate_first_gen(parameters["objects"], parameters["drones"])
+    print(waypoints)
     result = generate_paths_for_all_drones(parameters["drones"], waypoints)
-    simulation_json_generator(f"simulation_{filename}.json", result[0], result[1], parameters)
+    simulation_json_generator(f"../shared/simulation_{filename}.json", result[0], result[1], parameters)
